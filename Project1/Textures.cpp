e372982eab0275c9e5faa5a9ffd1df6d83815ccb
AchinthyaH <achinthyah@gmail.com>
2017-12-07 12:24:55 +0530

    Texture Code

--- Project1/Textures.cpp
+++ Project1/Textures.cpp
@@ -2,6 +2,7 @@
 #include <GL/glew.h>
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <SOIL.h>
 #include <iostream>
 using namespace std;
 
@@ -12,22 +13,29 @@ const GLchar* vertexSource = R"glsl(
     #version 150 core
     in vec2 position;
     in vec3 color;
+    in vec2 texcoord;
     out vec3 Color;
+    out vec2 Texcoord;
     void main()
     {
         Color = color;
+        Texcoord = texcoord;
         gl_Position = vec4(position, 0.0, 1.0);
     }
 )glsl";
 const GLchar* fragmentSource = R"glsl(
     #version 150 core
     in vec3 Color;
+    in vec2 Texcoord;
     out vec4 outColor;
+    uniform sampler2D tex;
     void main()
     {
-        outColor = vec4(Color, 1.0);
+        outColor = texture(tex, Texcoord) * vec4(Color, 1.0);
     }
 )glsl";
+
+
 
 int main(int argc, char *argv[])
 {
@@ -56,14 +64,28 @@ int main(int argc, char *argv[])
 	glGenBuffers(1, &vbo);
 
 	GLfloat vertices[] = {
-		0.0f,  0.5f, 1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f
+		//Position      Color         Texcoords
+		-0.5f,  0.5f,   1.0f, 0.0f,   0.0f, 0.0f, 0.0f, // Top-left
+		 0.5f,  0.5f,   0.0f, 1.0f,   0.0f, 1.0f, 0.0f, // Top-right
+		 0.5f, -0.5f,   0.0f, 0.0f,   1.0f, 1.0f, 1.0f, // Bottom-right
+		-0.5f, -0.5f,   1.0f, 1.0f,   1.0f, 0.0f, 1.0f  // Bottom-left
 	};
 
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Create an element array
+	GLuint ebo;
+	glGenBuffers(1, &ebo);
+
+	GLuint elements[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
 
 	// Create and compile the vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -83,14 +105,34 @@ int main(int argc, char *argv[])
 	glLinkProgram(shaderProgram);
 	glUseProgram(shaderProgram);
 
-	// Specify the layout of the vertex data
+	//Specify the layout of the vertex data
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
 
 	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
 	glEnableVertexAttribArray(colAttrib);
-	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+	GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+	glEnableVertexAttribArray(texAttrib);
+	glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+
+	// Load texture
+	GLuint tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+
+	int width, height;
+	unsigned char* image = SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 
 	while (true)
 	{
@@ -105,13 +147,14 @@ int main(int argc, char *argv[])
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw a triangle from the 3 vertices
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// Draw a rectangle from the 2 triangles using 6 indices
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Swap buffers
 		SDL_GL_SwapWindow(window);
 	}
 
+	glDeleteTextures(1, &tex);
 
 	glDeleteProgram(shaderProgram);
 	glDeleteShader(fragmentShader);
